b77a2259cb24aec54e266605839800605ad64acd
Isabellabmj <Isabellajoensen@gmail.com>
2026-02-13 08:53:25 +0100

    Initial Maven project with tests

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Maven!");
+        System.out.println("2 + 3 = " + add(2, 3));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
